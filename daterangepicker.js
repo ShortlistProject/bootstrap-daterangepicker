05a5414b327f16932b10b3f41828d30c1c525a2b
Konrad Siery <k.siery@gmail.com>
2020-02-25 22:35:46 +0100

    Not possible to select today as start and end date

--- daterangepicker.js
+++ daterangepicker.js
@@ -1060,7 +1060,7 @@
             }
 
             //if a new date range was selected, invoke the user callback function
-            if (!this.startDate.isSame(this.oldStartDate) || !this.endDate.isSame(this.oldEndDate))
+            // if (!this.startDate.isSame(this.oldStartDate) || !this.endDate.isSame(this.oldEndDate))
                 this.callback(this.startDate, this.endDate, this.chosenLabel);
 
             //if picker is attached to a text input, update it
